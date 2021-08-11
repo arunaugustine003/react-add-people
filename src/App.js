1338a6ee4f0e5d183c1d376d3a41050fa37fc81a
arunaugustine003 <arunaugustine003@gmail.com>
2021-08-11 21:43:57 +0530

    C2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
+
+  //Save Existing Todos to Local
+  const getLocalTodos = () => {
+    if (localStorage.getItem("usersList") === null) {
+      localStorage.setItem("usersList", JSON.stringify([]));
+    } else {
+      let usersListLocal = JSON.parse(localStorage.getItem("usersList"));
+      setUsersList(usersListLocal);
+    }
+  };
+  //Run once when the App Starts
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  //Save New Todos to Local Storage
+  const saveLocalTodos = () => {
+    localStorage.setItem("usersList", JSON.stringify(usersList));
+  };
+  //Run once when the App Starts
+  useEffect(() => {
+    saveLocalTodos();
+  }, [usersList]);
 
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
